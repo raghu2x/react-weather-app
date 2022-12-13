80e4c93d5edc3b711e077669fed8ffe0e4b5d74e
raghvendra <raghvendrayadav40771@gmail.com>
2022-12-13 22:45:13 +0530

    fixed deployment issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
-import AuthLayout from '../layouts/AuthLayout';
-import Signup from '../pages/Signup';
-import Login from '../pages/Login';
-import Home from '../pages/Home';
-import ForcastReport from '../pages/ForcastReport';
-import WeatherCard from '../compenents/WeatherCard';
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        children: [
-            {
-                path: "",
-                element: <WeatherCard />,
-            },
-            {
-                path: "forcast",
-                element: <ForcastReport />,
-            },
-        ]
-    },
-    {
-        path: "/",
-        element: <AuthLayout />,
-        children: [
-            {
-                path: "login",
-                element: <Login />,
-            },
-            {
-                path: "signup",
-                element: <Signup />,
-            },
-        ]
-    },
+import { createHashRouter } from "react-router-dom";
+import AuthLayout from "../layouts/AuthLayout";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import ForcastReport from "../pages/ForcastReport";
+import WeatherCard from "../compenents/WeatherCard";
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "",
+        element: <WeatherCard />,
+      },
+      {
+        path: "forcast",
+        element: <ForcastReport />,
+      },
+    ],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
 ]);
 
 export default router;
